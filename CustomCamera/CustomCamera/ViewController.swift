3266e899dd44878d6599cf9f6d0f108cd01229c5
Rodrigo Candido <rodrigoipu@hotmail.com>
2021-03-30 11:58:04 +0200

    On brach main - Initial commit Start Xcode empty project

---
+++ CustomCamera/CustomCamera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomCamera
+//
+//  Created by Rodrigo  Candido on 30/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
